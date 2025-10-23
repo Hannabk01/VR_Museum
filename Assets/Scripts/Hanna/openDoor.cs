73c2728714340835818e05a5d3acc55ddc1ae680
Hannabk01 <hannabk01@gmail.com>
2025-10-24 00:02:57 +0200

    Doors open

--- Assets/Scripts/Hanna/openDoor.cs
+++ Assets/Scripts/Hanna/openDoor.cs
@@ -1,59 +1,50 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class openDoor : MonoBehaviour
 {
     //Varibles
-    [SerializeField] float speed = 50f;
-    [SerializeField] float startRotation = 90f; // door y axis starts out at 90 when closed
-    [SerializeField] float endRotation = 10f; // door y axis ends up at 10 when open
-    [SerializeField] bool doorOpen = false; // door starts out closed
-    [SerializeField] private GameObject door ;
+    [SerializeField] private GameObject door;
+    [SerializeField] private UnityEvent onOpen;
+    [SerializeField] private UnityEvent onClose;
+    GameObject player;
+    AudioSource doorSound;
+    bool isOpen;
+    [SerializeField] float playerDistance = 0.01f;
 
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        doorSound = GetComponent<AudioSource>();
+        isOpen = false;
 
     }
 
 
-    // When object (player) intercepts trigger box
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (!isOpen)
         {
-            if (doorOpen)
-            {
-                door.transform.Rotate(0, startRotation, 0, Space.Self);
+            player = other.gameObject;
+            onOpen.Invoke();
+            doorSound.Play();
+            isOpen = true;
 
-                doorOpen = false;
-            }
-
-            else if (!doorOpen)
-            {
-                door.transform.Rotate(0, endRotation, 0, Space.Self);
-
-                doorOpen = true;
-            }
         }
     }
 
 
-    // When object (player) leaves trigger box
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.gameObject == player)
         {
-            
+            onClose.Invoke();
+            isOpen = false;
+
         }
-    }
-
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+            
     }
 }
+
